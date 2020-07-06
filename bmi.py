a6521ec77a9a4b70af9a6db8c1e43a533b077c70
yamami0301 <allstar.naikaku6@gmail.com>
2020-07-06 11:59:25 +0900

    ゲームのユーザー名を表現するUserNameクラスを実装
    ・文字数に関するルールをうまく1箇所に集めることができた
    ・`screen_name()`で管理したので、要件が変わっても変更がラク

---
+++ bmi.py
@@ -0,0 +1,32 @@
+'''
+class (成人の)BMI:
+    関連しそうな属性:
+        - 身長
+        - 体重
+        - BMIという値そのもの
+    ルール:
+        - 10以上40以下<-- 常識的な範囲
+        - 表示するときは、小数点第2位まで
+            - ex: 23.678 -> 23.67
+            - ex: 23.671 -> 23.67
+    できること:
+    - ???
+'''
+
+
+# クラス名はUpperCamelCaseが普通
+class BMI:
+    def __init__(self, height, weight):
+        self.height = height
+        self.weight = weight
+
+    def calculate_bmi(self):
+        return self.weight / (self.height ** 2)
+
+
+# BMIクラスのインスタンス化
+hibiki_bmi = BMI(height=1.80, weight=67.0)
+ohira_bmi = BMI(height=1.78, weight=75.0)
+
+print(hibiki_bmi.height, hibiki_bmi.weight)
+print(hibiki_bmi.calculate_bmi())
